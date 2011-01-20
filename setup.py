7c68f553562eb2b4191a092829b9b2b357d496f9
Dana Woodman <dana@danawoodman.com>
2011-01-20 12:46:11 -0800

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-ratings',
+    version = '0.1.0',
+    description = 'Generic Django reviews that can work on any model.',
+    author = 'Dana Woodman',
+    author_email = 'dana@danawoodman.com',
+    url = 'https://github.com/danawoodman/django-reviews',
+    packages = find_packages(),
+)
