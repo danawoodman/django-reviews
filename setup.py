8cc96608b6183ace78972070022f93edc83e0d5f
Dana Woodman <dana@danawoodman.com>
2011-01-20 12:47:54 -0800

    Fixing bug in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'django-ratings',
+    name = 'django-reviews',
     version = '0.1.0',
     description = 'Generic Django reviews that can work on any model.',
     author = 'Dana Woodman',
