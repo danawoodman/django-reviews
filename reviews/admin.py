ebf77f136646219be03a5205278416116a26d40f
Dana Woodman <dana@danawoodman.com>
2011-03-03 12:50:43 -0800

    Updating Review to ReviewedItem and making generic.

--- reviews/admin.py
+++ reviews/admin.py
@@ -9,10 +9,10 @@ class VoteAdmin(admin.ModelAdmin):
     search_fields = ('user',)
 
 
-class ReviewAdmin(admin.ModelAdmin):
+class ReviewedItemAdmin(admin.ModelAdmin):
     list_display = ('content_object', 'user', 'score', 'date_changed', 'date_added') #, 'ip_address'
     list_filter = ('score', 'date_changed', 'date_added', 'user') #, 'content_type'
     search_fields = ('user',)#, 'ip_address',)
 
 admin.site.register(Vote, VoteAdmin)
-admin.site.register(Review, ReviewAdmin)
+admin.site.register(ReviewedItem, ReviewedItemAdmin)
--- reviews/models.py
+++ reviews/models.py
@@ -25,7 +25,7 @@ class Vote(models.Model):
     user = models.ForeignKey(User, verbose_name=_('user'), 
         related_name='votes', blank=True, null=True)
     ip_address = models.IPAddressField(blank=True)
-    review = models.ForeignKey('Review', verbose_name=_('review'), 
+    review = models.ForeignKey('ReviewedItem', verbose_name=_('review'), 
         related_name='votes')
     date_added = models.DateTimeField(_('date added'), 
         default=datetime.datetime.now, editable=False)
@@ -60,33 +60,17 @@ class Vote(models.Model):
             self.review.id)
     
 
-class Review(models.Model):
+class ReviewedItem(models.Model):
     """
-    A generic Review of a Django Model.
-    
-    Usage::
-    
-        from django.db import models
-        
-        from reviews.models import Review
-        
-        class MyModel(models.Model):
-            foo = models.TextField()
-            reviews = models.ForeignKey(Review)
-    
+    A generic ReviewedItem of a Django Model.
     """
     content_type = models.ForeignKey(ContentType, verbose_name=_('content type'), 
         related_name="reviews")
-    object_id = models.PositiveIntegerField(_('object ID'))
-    
-    # key = models.CharField(_('key'), max_length=32)
+    object_id = models.PositiveIntegerField(_('object ID')) 
     score = models.IntegerField(_('rating'))
     content = models.TextField(_('review'))
     user = models.ForeignKey(User, verbose_name=_('user'), 
         related_name='reviews')
-    # votes = models.ManyToManyField(Vote, verbose_name=_('reviews'), 
-    #     related_name='reviews')
-    # ip_address = models.IPAddressField(_('IP address'))
     date_added = models.DateTimeField(_('date added'), 
         default=datetime.datetime.now, editable=False)
     date_changed = models.DateTimeField(_('date changed'), 
@@ -94,34 +78,21 @@ class Review(models.Model):
     
     content_object = generic.GenericForeignKey()
     
-    objects = ReviewManager()
+    objects = ReviewedItemManager()
     
     # TODO: Add flagging of reviews...
     
-    # def user_display(self):
-    #     if self.user:
-    #         return "%s (%s)" % (self.user.username, self.ip_address)
-    #     return self.ip_address
-    # user_display = property(user_display)
-    # 
-    # def partial_ip_address(self):
-    #     ip = self.ip_address.split('.')
-    #     ip[-1] = 'xxx'
-    #     return '.'.join(ip)
-    # partial_ip_address = property(partial_ip_address)
-    # 
     class Meta:
-        verbose_name = _('review')
-        verbose_name_plural = _('reviews')
+        verbose_name = _('reviewed item')
+        verbose_name_plural = _('reviewed items')
         # unique_together = (('content_type', 'object_id', 'key', 'user')) #, 'ip_address'
     
     def save(self, *args, **kwargs):
         self.date_changed = datetime.datetime.now()
-        super(Review, self).save(*args, **kwargs)
+        super(ReviewedItem, self).save(*args, **kwargs)
     
     def __unicode__(self):
         return u"Review of %s by %s" % (self.content_object, self.user.username)
-        # return u"%s voted %s on %s" % (self.user_display, self.score, self.content_object)
 
 
     
