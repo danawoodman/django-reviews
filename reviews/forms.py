80697b8461f31852a5bc806093a1a92f418e8ddb
Dana Woodman <dana@danawoodman.com>
2011-03-05 12:15:24 -0800

    Making the score field be a radio select in ReviewedItemForm.

--- reviews/forms.py
+++ reviews/forms.py
@@ -2,18 +2,26 @@ from django import forms
 from django.forms import ModelForm
 from django.utils.translation import ugettext_lazy as _
 
-from reviews.models import Review
+from reviews.models import ReviewedItem
 
 
-class ReviewForm(ModelForm):
+class ReviewedItemForm(ModelForm):
     """
-    The Review add/edit form.
+    The ReviewedItem add/edit form.
     """
-    score = forms.IntegerField(label=_('Rating'), required=True, max_value=5)
+    SCORE_CHOICES = (
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+    )
+    score = forms.ChoiceField(label=_('Rating'), required=True, 
+        choices=SCORE_CHOICES, widget=forms.RadioSelect(attrs={'class': 'star'}))
     content = forms.CharField(label=_('Review'), required=True, help_text='', widget=forms.Textarea)
         
     class Meta:
-        model = Review
+        model = ReviewedItem
         fields = (
             'score',
             'content',
